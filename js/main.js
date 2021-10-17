106df296ccfa0549dc6a527131eaf9eb916e1263
ashwin <ashu61998@gmail.com>
2021-10-17 06:05:05 +0530

    final changes

--- js/main.js
+++ js/main.js
@@ -5,18 +5,21 @@
         Thing1 : {
             name: "CINEMA",
             description: "I love films so much that i watch films pretty much every weekend. I watch that when i feel sad or bored or happy or in any mood i am in.",
+            recommendation: "My recommendation: Blade runner, Intersetllar, Pulp fiction",
             image: "cinema.jpeg"
         },
 
         Thing2 : {
             name: "MUSIC",
-            description: "dasdsadas",
+            description: "I listen to music pretty much when i work and when i am relaxing a bit. I love to have some music playing in the background when i am working. It speeds up my process (or that's what i feel like)",
+            recommendation: "My recommendation: Bangarang, Perfect, Always remember us this way",
             image: "music.jpg"
         },
 
         Thing3 : {
             name: "VIDEO GAMES",
-            description: "adssdads",
+            description: "When i am totally free and there is nothing important at the moment, i play games then. I have grown up playing games and that's something i always love to do no matter what. Even though i end up raging when i play online games, i love it",
+            recommendation: "My recommendation: Valorant, Apex legends, Witcher",
             image: "video_games.jpg"
         }
 
@@ -28,6 +31,7 @@
     //things = {};
     const theThing = document.querySelector("#favorite-items"),
     theDescrip = document.querySelector("p"),
+    theRecommend = document.querySelector(".recommendation"),
     theImage = document.querySelector(".image img")
 
 function showData(){
@@ -35,6 +39,7 @@ function showData(){
     theImage.src = `images/${things[key].image}`;
     theThing.textContent = things[key].name;
     theDescrip.textContent = things[key].description;
+    theRecommend.textContent = things[key].recommendation;
 }
 
 
